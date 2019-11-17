f40cd9c0b9dd2c8cd6b02f9ef57a3a07fef493c0
YusufSyam <54948391+YusufSyam@users.noreply.github.com>
2019-11-17 17:02:49 +0800

    Update Modus.java
    Program mencari modus dari sebuah data tunggal

--- Modus.java
+++ Modus.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Modus {
 	
 	// Mencari modus dari sebuah data tunggal
-	
+	 
 	static Scanner xx= new Scanner(System.in);
 	public static void main(String [] args) {
 		ArrayList <Integer> bilanganSama= new ArrayList<>();
